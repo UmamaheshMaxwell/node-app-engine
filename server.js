de95eef6eea972bf04ce970fd5696fe2797a8762
Umamaheswararao Meka <maheshmeka16@gmail.com>
2024-09-11 19:02:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// app.get('/', (req, res) => {
+//    res.send('<html><body><h1>Welcome to App Engine Deployment</h1></body></html>');
+//});
+
 app.get('/', (req, res) => {
-    res.send('<html><body><h1>Welcome to App Engine Deployment</h1></body></html>');
+    res.send('Hello, CI/CD Pipeline with GitHub Actions!');
 });
   
 
